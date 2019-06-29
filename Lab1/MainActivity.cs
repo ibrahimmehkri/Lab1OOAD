2809cf9e15113c60bf912c287da25d9b122710ff
Ibrahim Mehkri <ibrahimmehkri@gmail.com>
2019-06-29 18:11:07 +0200

    First Commit

---
+++ Lab1/MainActivity.cs
@@ -0,0 +1,85 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic; 
+
+namespace Lab1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        Command command; 
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            TextView textView = FindViewById<TextView>(Resource.Id.textView1);
+            EditText editText = FindViewById<EditText>(Resource.Id.editText1);
+            command = new PrintCommand(textView, editText); 
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += delegate
+            {
+                command.Execute();
+            };
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            button2.Click += delegate
+            {
+                command.Undo();
+            };
+            
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        
+    }
+    interface Command
+    {
+        void Execute();
+        void Undo();
+    }
+
+    class PrintCommand : Command
+    {
+        TextView textView;
+        EditText editText;
+        List<string> list = new List<string>(); 
+
+        public PrintCommand(TextView textView, EditText editText)
+        {
+            this.textView = textView;
+            this.editText = editText; 
+        }
+
+        public void Execute()
+        {
+            string text = editText.Text;
+            list.Add(textView.Text);
+            textView.Text = text; 
+        }
+
+        public void Undo()
+        {
+            string previousText = "";
+
+            if(list.Count > 0)
+            {
+                previousText = list[list.Count - 1];
+                list.RemoveAt(list.Count - 1); 
+            }
+
+            textView.Text = previousText; 
+        }
+    }
+}
---
+++ Lab1/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "8e96b2ba-4677-47be-8e36-58f587ddde27",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "System.Net.Http.HttpClientHandler, System.Net.Http",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
